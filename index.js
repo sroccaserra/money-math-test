57475e187c8a6a2f5ffe23dc259fa67943a9268e
Sébastien Roccaserra <sroccaserra@yahoo.com>
2016-07-18 18:29:49 +0200

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const money = require('money-math');
+const test = require('tape');
+
+
+test('javascript has a floating point problem', t => {
+    t.equal(0.060000000000000005, 0.01 + 0.05);
+    t.end();
+});
+
+test('money-math fixes this floating point problem', t => {
+    t.equal('0.06', money.add('0.01', '0.05'));
+    t.end();
+});
