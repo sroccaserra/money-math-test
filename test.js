cd5c1043a0cd2e15f5ccffb753df0bf4f86ff3bd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2016-07-19 13:46:37 +0200

    changed examples

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const money = require('money-math')
 const test = require('tape')
 
 test('javascript has a floating point problem', t => {
-    t.equal(0.060000000000000005, 0.01 + 0.05)
+    t.equal(0.10 + 0.20, 0.30000000000000004)
     t.end()
 })
 
 test('money-math fixes this floating point problem', t => {
-    t.equal('0.06', money.add('0.01', '0.05'))
+    t.equal(money.add('0.10', '0.20'), '0.30')
     t.end()
 })
