872632fd098b3cb675904027f9b0dbffe8c1d197
Sébastien Roccaserra <sroccaserra@yahoo.com>
2016-07-20 16:02:32 +0200

    added a test on numeraljs

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const money = require('money-math')
+const numeral = require('numeral')
 const test = require('tape')
 
 test('javascript has a floating point problem', t => {
@@ -8,5 +9,11 @@ test('javascript has a floating point problem', t => {
 
 test('money-math fixes this floating point problem', t => {
     t.equal(money.add('0.10', '0.20'), '0.30')
+    t.end()
+})
+
+test('numeral fixes this floating point problem too', t => {
+    const x = numeral(0.2)
+    t.equal(x.add(0.1).value(), 0.3)
     t.end()
 })
